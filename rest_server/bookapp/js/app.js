d7e0bbea0ed7e8b159903a79bf6f197c6fff397f
pautomaszewska <ptomaszewska@gmail.com>
2019-02-21 20:34:34 +0100

    Added new functions

--- rest_server/bookapp/js/app.js
+++ rest_server/bookapp/js/app.js
@@ -1,64 +1,79 @@
-$.ajax({
-    url: 'http://127.0.0.1:8000/book',
-    data: {},
-    type: "GET",
-    dataType: "json"
-}).done(function (result) {
+$(document).ready( function () {
+
+    var callAjax = function (url, data, type, fun) {
+        $.ajax({
+            url: url,
+            data: data,
+            type: type,
+            dataType: "json"
+        }).done(fun)
+    };
+
+    var baseUrl = 'http://127.0.0.1:8000/book/';
     var doc = $('.books');
 
-    for (var i = 0; i < result.length; i++) {
-        // console.log(result[i].author, result[i].title);
-        var title = $('<p></p>').attr('id', result[i].id);
-        // title.data('id', result[i].id);
-        title.text(result[i].title);
-        doc.append(title);
+    var showBooks = function (result) {
 
-        var description = $('<div></div>');
-        doc.append(description);
-        description.hide();
+        for (var i = 0; i < result.length; i++) {
+            var title = $('<li></li>').attr('id', result[i].id).addClass('position');
 
+            title.text(result[i].title);
 
-        title.on('click', function () {
-            var id = $(this).attr('id');
-            var desc = $(this).next();
-            desc.toggle();
+            var del = $('<p>Usuń</p>').addClass('delete');
+            title.append(del);
 
+            doc.append(title);
 
-            $.ajax({
-                url: 'http://127.0.0.1:8000/book/' + id,
-                data: {},
-                type: "GET",
-                dataType: "json"
-            }).done(function (result) {
+            var description = $('<div></div>');
+            doc.append(description);
+            description.hide();
 
-                var bookInfo = $('<p>Autor: ' + result.author + '</p>'
-                + '<p>ISBN: ' + result.isbn + '</p>' + '<p>Wydawca: ' + result.publisher + '</p><br>');
+            title.on('click',function () {
 
-                desc.html(bookInfo);
-            })
+                    var id = $(this).attr('id');
+                    var desc = $(this).next();
+
+                    var bookDetails = function (result) {
+                        var bookInfo = $('<p>Autor: ' + result.author + '</p>'
+                         + '<p>ISBN: ' + result.isbn + '</p>' + '<p>Wydawca: ' + result.publisher + '</p>'
+                         + '<p>Gatunek: ' + result.genre + '</p><br>');
+
+                        desc.html(bookInfo);
+                };
+                    callAjax((baseUrl + id), {}, "GET", bookDetails);
+                     desc.toggle();
+            });
+
+            del.on('click',function () {
+            var id = $(this).parent().attr('id');
+            var deleteBook = function () {
+                alert('Usunięto wybraną książkę');
+                location.reload(true)
+            };
+            callAjax((baseUrl + id), {}, "DELETE", deleteBook());
+        });
+        }
+    };
+
+        var submit = $('button');
+        var form = $('form');
+
+        submit.on('click', function (event) {
+            event.preventDefault()
+            var info = {
+                title: form.find('#title').val(),
+                author: form.find('#author').val(),
+                isbn: form.find('#isbn').val(),
+                publisher: form.find('#publisher').val(),
+                genre: form.find('#genre').val()
+            };
+            var addBook = function () {
+                 alert('Dodano książkę');
+                 location.reload(true)
+            };
+            callAjax(baseUrl, info, "POST", addBook);
         });
 
+        callAjax(baseUrl,{}, 'GET', showBooks);
 
-        var form = $('input');
-        var submit = $('button');
-        var info = {
-            title: form.find('[name="title"]'),
-            author: form.find('[name="author"]'),
-            isbn: form.find('[name="isbn"]'),
-            publisher: form.find('[name="title"]')
-        };
-
-        $.ajax({
-            url: 'http://127.0.0.1:8000/book/',
-            data: {},
-            type: "POST",
-            dataType: "json"
-        }).done(function (result) {
-
-        })
-    }
-})
-
-
-
-
+ });
